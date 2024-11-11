2cea7aa70b848414345b01a46f60e32091d50f91
nvanzeijl <nvanzeijl@gmail.com>
2024-11-11 22:49:46 +0100

    opdracht 2 f al klaar. Nog doen 2d en 2e

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import './App.css';
 import {soldTelevisions} from './helpers/totalSoldTVs.js';
 import {stockStart} from './helpers/startStock.js';
 import {stockCurrent} from './helpers/currentStock.js';
-import {nameTv} from './helpers/nametv.js';
-import {priceTv} from './helpers/tvPrice.js';
-import {sizeOfTv} from './helpers/tvSize.js';
 
 function App() {
     const resultSoldTv = soldTelevisions();
     const resultStartStock = stockStart()
     const resultCurrentStock = stockCurrent();
 
+    function printText(buttonText) {
+        console.log(buttonText);
+    }
 
     return (
         <>
@@ -20,22 +20,32 @@ function App() {
 
             <main>
                 <section className="verkoopoverzicht">
-                <h2>Verkoopoverzicht</h2>
-                <div>
-                    <p className="soldTvs">Aantal verkochte producten: {resultSoldTv}</p>
-                </div>
-                <div>
-                    <p className="startStock">Aantal ingekochte producten: {resultStartStock}</p>
-                </div>
-                <div>
-                    <p className="currentStock">Aantal te verkopen producten: {resultCurrentStock}</p>
-                </div>
+                    <h2>Verkoopoverzicht</h2>
+                    <div>
+                        <p className="soldTvs">Aantal verkochte producten: {resultSoldTv}</p>
+                    </div>
+                    <div>
+                        <p className="startStock">Aantal ingekochte producten: {resultStartStock}</p>
+                    </div>
+                    <div>
+                        <p className="currentStock">Aantal te verkopen producten: {resultCurrentStock}</p>
+                    </div>
                 </section>
 
                 <section className="best-sold-Tv">
-
+                    <h3>best sold tv info here</h3>
 
                 </section>
+                <section>
+                    <button type="button" onClick={() => printText('Meest verkochte eerst')}>Meest verkochte eerst
+                    </button>
+                    <button type="button" onClick={() => printText('Goedkoopste eerst')}>Goedkoopste eerst
+                    </button>
+                    <button type="button" onClick={() => printText('Meest geschikt voor sport eerst')}>Meest geschikt
+                        voor sport eerst
+                    </button>
+                </section>
+
             </main>
 
         </>
